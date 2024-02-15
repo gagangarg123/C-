91e64f7c571f5a3ec078b8b6eb547cb7ed8153aa
gagangarg123 <150772040+gagangarg123@users.noreply.github.com>
2024-02-15 20:35:35 +0530

    Add files via upload

---
+++ pointers.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+    /*
+    int *p;
+    int a=10;
+    p=&a;
+    printf("%d\n",a);
+    printf("%d\n",*p);
+    printf("%d\n",p);
+    printf("%d",&a);
+    */
+   
+}
